2ecc71386266c9003ee1d14c10506998a369edba
Ryota Konno <r.konno@nowall.co.jp>
2017-07-15 21:57:16 +0900

    [add] Aboutページを作成（モバイルファースト）

---
+++ js/simple_page_scripts.js
@@ -0,0 +1,40 @@
+$(function() {
+
+    'use strict';
+
+    // --------------------------------------------------------------------
+    // One Page Navigation
+    // --------------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() >= 50) {
+                $('nav.navbar').addClass('sticky-nav');
+            }
+            else {
+                $('nav.navbar').removeClass('sticky-nav');
+            }
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // Closes the Responsive Menu on Menu Item Click
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('.navbar-collapse ul li a').on('click', function () {
+            if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+                $('.navbar-toggle:visible').trigger('click');
+            }
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // particale.js
+    // --------------------------------------------------------------------
+    particlesJS.load('background-particles', '/js/particles.json', function() {
+      // customize file -> particles.json
+      console.log('particle.js loaded!');
+    });
+
+}); // JQuery end
